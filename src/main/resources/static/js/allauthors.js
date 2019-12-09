6f747c5c73e9e8cd77f7ca5cb604434937ee38e0
EIK <petrodarchyn@gmail.com>
2019-12-09 21:26:00 +0200

    xml saving/loading authors

--- src/main/resources/static/js/allauthors.js
+++ src/main/resources/static/js/allauthors.js
@@ -10,8 +10,115 @@ $(document).ready(function(){
         var table = document.getElementById("allBooksTable");
         sortTable(table, 2);
     });
+    $('#saveXML').on('click', function(){
+        var xml = returnTableAsXML();
+        download("text.xml",xml);
+    });
+    $('#loadXML').on('click', function(){
+        console.log("click");
+        $("#my_file").val('');
+        document.getElementById('my_file').click();
+    });
+     $('#my_file').on('change', function() {
+        console.log("herere");
+    	// list of selected files
+    	var all_files = this.files;
+    	if(all_files.length == 0) {
+    		alert('Error : No file selected');
+    		return;
+    	}
 
+    	// first file selected by user
+    	var file = all_files[0];
+
+    	// Max 2 MB allowed
+    	var max_size_allowed = 2*1024*1024
+    	if(file.size > max_size_allowed) {
+    		alert('Error : Exceeded size 2MB');
+    		return;
+    	}
+
+    	// file validation is successful
+    	// we will now read the file
+    	var reader = new FileReader();
+
+        // file reading started
+        reader.addEventListener('loadstart', function() {
+            console.log('File reading started');
+        });
+
+        // file reading finished successfully
+        reader.addEventListener('load', function(e) {
+            var text = e.target.result;
+
+            // contents of the file
+            populateTableWithXML(text);
+        });
+
+        // file reading failed
+        reader.addEventListener('error', function() {
+            alert('Error : Failed to read file');
+        });
+
+        // file read progress
+        reader.addEventListener('progress', function(e) {
+            if(e.lengthComputable == true) {
+            	var percent_read = Math.floor((e.loaded/e.total)*100);
+            	console.log(percent_read + '% read');
+            }
+        });
+
+        // read as text file
+        reader.readAsText(file);
+    });
 });
+function populateTableWithXML(xml){
+    var xmlDoc = jQuery.parseXML(xml);
+    console.log(xmlDoc.getElementsByTagName("author"));
+    var authors = xmlDoc.getElementsByTagName("author");
+    var table = document.getElementById("allAuthorsTable");
+    var tbody = table.getElementsByTagName('tbody')[0];
+    tbody.innerHTML = "";
+    for(var i =0; i < authors.length; i++){
+         var name = authors[i].getAttribute("name");
+         var id = authors[i].getAttribute("id");
+         var newRow = tbody.insertRow(table.length);
+         var cell = newRow.insertCell(0);
+         cell.innerHTML = "<a class=\"btn btn-default\" href=\"/admin/authors/edit?id="+id+"\">" +
+         "<i class=\"fa fa-wrench\" ></i></a>";
+         cell.setAttribute("class", "actionCell");
+         cell = newRow.insertCell(1);
+         cell.innerHTML = id;
+         cell.setAttribute("class", "idCell");
+         cell = newRow.insertCell(2);
+         cell.innerHTML = name;
+         cell.setAttribute("class", "nameCell");
+    }
+}
+function returnTableAsXML(){
+    var xml = "<items>\n";
+        $("#allAuthorsTable tbody tr").each(function() {
+            var cells = $("td", this);
+            if (cells.length > 0) {
+                xml += "<author name='" + cells.eq(2).text() + "'"+ " id='" + cells.eq(1).text() +"'>\n";
+                xml += "</author>\n";
+            }
+        });
+        xml+= "</items>"
+        return xml;
+}
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
 function requestAuthors(){
  $.ajax({
         type: "GET",
